369b194ff8addca7f483fd0357533179be9c5ed0
sFlcn <57070095+sFlcn@users.noreply.github.com>
2020-08-16 23:00:57 +0300

    Скрипт анимации появления контента при скроле

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// анимация появления контента при скроле
+
+var isScrolling = false;
+
+window.addEventListener("scroll", throttleScroll, false);
+
+function throttleScroll(e) {
+  if (isScrolling == false) {
+    window.requestAnimationFrame(function() {
+      scrolling(e);
+      isScrolling = false;
+    });
+  }
+  isScrolling = true;
+}
+
+document.addEventListener("DOMContentLoaded", scrolling, false);
+
+var listItems = document.querySelectorAll(".animated-appearance");
+
+function scrolling(e) {
+  for (var i = 0; i < listItems.length; i++) {
+    var listItem = listItems[i];
+    if (isPartiallyVisible(listItem)) {
+      listItem.classList.add("animated-appearance--shown");
+    } 
+  }
+}
+
+function isPartiallyVisible(el) {
+  var elementBoundary = el.getBoundingClientRect();
+
+  var top = elementBoundary.top;
+  var bottom = elementBoundary.bottom;
+  var height = elementBoundary.height;
+
+  return (height + window.innerHeight >= bottom) /* && (top + height >= 0)*/ ;
+}
+
+function isFullyVisible(el) {
+  var elementBoundary = el.getBoundingClientRect();
+
+  var top = elementBoundary.top;
+  var bottom = elementBoundary.bottom;
+
+  return ((top >= 0) && (bottom <= window.innerHeight));
+}
